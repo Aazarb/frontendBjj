b5a49cb9f48dc801633eee7f419fc96b8b9cfd7c
Aazarb <zaraba.adil@hotmail.fr>
2025-01-25 23:44:22 +0100

    Add .environment files
    - angular.json assets updated
    - ngx-dotenv removed to package.json
    - .gitignore updated
    - auth service updated
    - Role and authentication api call added

--- main/src/app/services/auth.service.ts
+++ main/src/app/services/auth.service.ts
@@ -1,32 +1,63 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Router} from "@angular/router";
-// @ts-ignore
-import { NgxEnvService } from 'ngx-dotenv';
-import {BehaviorSubject} from "rxjs";
+import { environment } from '../../environments/environment.development';
+import {BehaviorSubject, catchError, map, Observable, of} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
   private apiUrl: string;
+  private roleSubject = new BehaviorSubject<string | null>(null);
 
-  constructor(private http: HttpClient, private router: Router, private envService: NgxEnvService) {
-    this.apiUrl = this.envService.get('API_URL') || 'https://localhost:8081/auth';
+  constructor(private http: HttpClient) {
+    this.apiUrl = environment.apiUrl || 'https://localhost:8081/auth';
     console.log('API_URL:', this.apiUrl);
   }
 
-  login(credentials: { email: string, password: string }) {
-    return this.http.post(`${this.apiUrl}/login`, credentials, { withCredentials: true })
-      .subscribe(() => {
-        this.router.navigate(['/dashboard']);
-      });
+  login(credentials: { email: string, password: string }): Observable<boolean> {
+    return this.http.post<{ role: string }>(`${this.apiUrl}/login`, credentials, { withCredentials: true }).pipe(
+      map(response => {
+        if (response && response.role) {
+          this.roleSubject.next(response.role); // Stocke le rôle
+          return true;
+        }
+        return false;
+      }),
+      catchError(() => {
+        return of(false);
+      })
+    );
   }
 
   logout() {
-    return this.http.post(`${this.apiUrl}/logout`, {}, { withCredentials: true })
+    return this.http.post(`${this.apiUrl}/logout`, {}, {withCredentials: true})  // withCredentials pour la gestion du token coté backend
       .subscribe(() => {
-        this.router.navigate(['/login']);
+        this.roleSubject.next(null);
       });
+  }
+
+  /** Observable pour suivre les rôles en temps réel */
+  get role$(): Observable<string | null> {
+    return this.roleSubject.asObservable();
+  }
+
+  /**  Retourne le rôle actuel */
+  getRole(): string | null {
+    return this.roleSubject.value;
+  }
+
+  /**  Retourne l'etat d'authentification connexion et le role actuel */
+  checkAuthStatusAndUpdateRole(): Observable<boolean> {
+    return this.http.get<{ authenticated: boolean, role: string }>(`${this.apiUrl}/status`, {withCredentials: true}).pipe(
+      map(response => {
+          if (response && response.role) {
+            this.roleSubject.next(response.role);
+            return true;
+          }
+          return false;
+        }),
+      catchError(() => of(false)),
+    );
   }
 }
